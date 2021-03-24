365cd82d47f810f8d8899e40c0f4dde773cb3aea
Jean <jm@mira.org>
2021-03-24 07:55:30 -0700

    Added to isntall script to replace automatically update root path for Python verison

--- install_unix.sh
+++ install_unix.sh
@@ -6,6 +6,8 @@ flag2=0
 flag3=0
 flag4=0
 flag5=0
+flag6=0
+flag7=0
 
 if [ $# -eq 0 ]; then
     echo "Usage: ./install_unix.sh [--diamonds | -d] [--background | -b] [--peakbagging | -p] [--asymptotic | -a] [--parallel | -g]"
@@ -46,8 +48,11 @@ do
 		--parallel | -g)
 			flag5=1
 			;;
+		--python | -y)
+			flag6=1
+			;;
 		*) 
-			echo "Flag $1 not recognized. Only flags -d, -b, -p, -a, -g are allowed. Aborting..."
+			echo "Flag $1 not recognized. Only flags -d, -b, -p, -a, -g -y are allowed. Aborting..."
 			exit 1
 			;;
 	esac
@@ -192,3 +197,13 @@ echo "----------------------------------------------------------------------"
 echo " Changing configuring parameters of the pipeline with local folders..."
 echo "----------------------------------------------------------------------"
 sed -i.old "s^YOUR_LOCAL_ROOT_PATH_HERE^${PWD}^g" ${PWD}/FAMED/idl/famed_configuring_parameters.txt
+
+sed -i.old "s^YOUR_LOCAL_ROOT_PATH_HERE^${PWD}^g" ${PWD}/FAMED/python/famed/famed/famed_configuring_parameters.txt
+sed -i.old "s^YOUR_LOCAL_ROOT_PATH_HERE^${PWD}^g" ${PWD}/FAMED/python/famed/famed/famed_config.yml
+
+echo "----------------------------------------------------------------------"
+echo " To use the python version, please add:"
+echo ${PWD}/FAMED/python/famed 
+echo "to your $PYTHONPATH environment variable"
+
+echo "----------------------------------------------------------------------"
